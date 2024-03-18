39b2963fc0299f9cc1d9af40b60cc8f67907ad69
Ekaterina <kateshenyang@gmail.com>
2024-03-18 21:57:31 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+  
+    dropdowns.forEach(dropdown => {
+      const valueElement = dropdown.querySelector('.dropdown__value');
+      const listElement = dropdown.querySelector('.dropdown__list');
+      const itemElements = dropdown.querySelectorAll('.dropdown__item');
+
+      valueElement.addEventListener('click', () => {
+        listElement.classList.toggle('dropdown__list_active');
+      });
+
+      itemElements.forEach(item => {
+        item.addEventListener('click', (event) => {
+          event.preventDefault();
+
+          listElement.classList.remove('dropdown__list_active');
+
+          const newValue = item.querySelector('.dropdown__link').textContent;
+          valueElement.textContent = newValue;
+        });
+      });
+    });
+  });
