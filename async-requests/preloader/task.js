6a64ccef78652a6bc8ac98f800925c1d1755b702
Ekaterina <kateshenyang@gmail.com>
2024-04-04 20:33:23 +0300

    preloader commit 2

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,53 +1,25 @@
-export default async function run({execution_id, input, data}) {
-    const loadCurrencyRates = () => {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses', true);
-
-            request.onload = function() {
-                if (this.status >= 200 && this.status < 300) {
-                    resolve(JSON.parse(this.response));
-                } else {
-                    reject({
-                        status: this.status,
-                        statusText: this.statusText
-                    });
-                }
-            };
-
-            request.onerror = function() {
-                reject({
-                    status: this.status,
-                    statusText: this.statusText
-                });
-            };
-
-            request.send();
-        });
-    };
-
-    try {
-        const response = await loadCurrencyRates();
-        const valutes = response.response.Valute;
-        const itemsContainer = document.getElementById('items');
-        itemsContainer.innerHTML = '';
-
-        Object.values(valutes).forEach(valute => {
-            const itemElement = document.createElement('div');
-            itemElement.classList.add('item');
-            itemElement.innerHTML = `
-                <div class="item__code">${valute.CharCode}</div>
-                <div class="item__value">${valute.Value}</div>
-                <div class="item__currency">руб.</div>
-            `;
-            itemsContainer.appendChild(itemElement);
-        });
-
-        document.getElementById('loader').classList.remove('loader_active');
-    } catch (error) {
-        console.error('Ошибка при загрузке данных:', error);
-        document.getElementById('loader').classList.remove('loader_active');
+const loader = document.getElementById('loader')
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', () => {
+    if (request.readyState === request.DONE){
+        loader.classList.remove('loader_active')
+        let data = JSON.parse(request.responseText)
+        const item = document.getElementById('items')
+        Object.keys(data.response.Valute).forEach(function(key) {
+            item.innerHTML +=
+            `<div class="item">
+            <div class="item__code">
+                ${data.response.Valute[key].CharCode}
+            </div>
+            <div class="item__value">
+                ${data.response.Valute[key].Value}
+            </div>
+            <div class="item__currency">
+               руб.
+            </div>
+            </div>`
+        })
     }
-
-    return {};
-}
+})
+request.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses')
+request.send()
